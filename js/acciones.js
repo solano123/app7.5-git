abf4474b39342b769cffb68191d771e6a1a8549f
solano123 <38224628+solano123@users.noreply.github.com>
2018-05-23 17:47:49 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,22 +1,24 @@
 // JavaScript Document
-$(document).ready(function(e) {
-	document.addEventListener("deviceready", function(){
+
+$(document).ready(function (e) {
+	document.addEventListener("Deviceready",function(){
+		
 		$('#izquierda').on("swipeleft",function(){
-			navigator.notification.alert("Deslizó a la izquierda",function(){"aplicación7","Aceptar"});
-		}); //cierre deslizar izquierda
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Aplicacion7","Aceptar"});
+		});
 		$('#derecha').on("swiperight",function(){
-			navigator.notification.confirm("¿Que quieres hacer?",function(opt){
+			navigator.notification.confirm("¿Qué quieres hacer?",function(opt){
 				switch(opt)
 				{
 					case 1:
-					navigator.notification.beep(2);
+					navigator.notification.beep(1);
 					break;
 					
-					case 2:
-					navigator.notification.vibrate(2000);
+					case2:
+					navigator.notification.vibrate(1000);
 					break;
 				}
-			},"aplicación7","Beepear,Vibrar,Cancelar");
-		});// cierre del deslizar derecha
-	}, false);// cierre del deviceready
-});// cierre del document principal
+			},"Aplicacion7","Beep,Vibrar,Cancelar");	
+	});
+},false);
+});
